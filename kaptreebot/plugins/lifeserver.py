0875eefc9ff40667ec0582da164a51d330c61bd5
uxiahnan <uxiahnan@outlook.com>
2021-06-06 05:33:07 +0800

    格式修改

--- kaptreebot/plugins/lifeserver.py
+++ kaptreebot/plugins/lifeserver.py
@@ -4,7 +4,7 @@ from nonebot import on_command
 from nonebot.adapters.cqhttp import Bot, Event
 
 
-epidemic = on_command("国内疫情", priority=2)
+epidemic = on_command("新冠疫情", priority=2)
 
 
 @epidemic.handle()
@@ -35,51 +35,51 @@ def get_news():
             results += n['infoSource'] + '\n'
             results += '\n---------------------------\n\n'
         desc = news['desc']
-        results += '累计确诊：' + str(desc['confirmedCount']) + '\n'
-        results += '现存确诊：' + str(desc['currentConfirmedCount']) + '\n'
-        results += '疑似确诊：' + str(desc['suspectedCount']) + '\n'
-        results += '治愈病例：' + str(desc['curedCount']) + '\n'
-        results += '死亡病例：' + str(desc['deadCount']) + '\n'
-        results += '无症状感染：' + str(desc['seriousCount']) + '\n'
+        results += '[累计确诊]：' + str(desc['confirmedCount']) + '\n'
+        results += '[现存确诊]：' + str(desc['currentConfirmedCount']) + '\n'
+        results += '[疑似确诊]：' + str(desc['suspectedCount']) + '\n'
+        results += '[治愈病例]：' + str(desc['curedCount']) + '\n'
+        results += '[死亡病例]：' + str(desc['deadCount']) + '\n'
+        results += '[无症状感染]：' + str(desc['seriousCount']) + '\n'
         results += '\n---------------------------\n\n'
         if desc['remark1'] != '':
-            results += desc['remark1'] + '\n'
+            results += desc['remark1'] + '\n\n'
         if desc['remark2'] != '':
-            results += desc['remark2'] + '\n'
+            results += desc['remark2'] + '\n\n'
         if desc['remark3'] != '':
-            results += desc['remark3'] + '\n'
+            results += desc['remark3'] + '\n\n'
         if desc['remark4'] != '':
-            results += desc['remark4'] + '\n'
+            results += desc['remark4'] + '\n\n'
         if desc['remark5'] != '':
-            results += desc['remark5'] + '\n'
+            results += desc['remark5'] + '\n\n'
         if desc['note1'] != '':
-            results += desc['note1'] + '\n'
+            results += desc['note1'] + '\n\n'
         if desc['note2'] != '':
-            results += desc['note2'] + '\n'
+            results += desc['note2'] + '\n\n'
         if desc['note3'] != '':
-            results += desc['note3'] + '\n'
+            results += desc['note3'] + '\n\n'
         if desc['generalRemark'] != '':
-            results += desc['generalRemark'] + '\n'
+            results += desc['generalRemark'] + '\n\n'
         if desc['abroadRemark'] != '':
-            results += desc['abroadRemark'] + '\n'
+            results += desc['abroadRemark'] + '\n\n'
         # 海外疫情
         results += '\n======海外疫情======\n\n'
         foreignStatistics = desc['foreignStatistics']
         results += '[累计确诊]：' + \
             str(foreignStatistics['confirmedCount']) + '\n'
-        results += '现存确诊：' + \
+        results += '[现存确诊]：' + \
             str(foreignStatistics['currentConfirmedCount']) + '\n'
-        results += '疑似病例：' + \
+        results += '[疑似病例]：' + \
             str(foreignStatistics['suspectedCount']) + '\n'
-        results += '治愈病例：' + str(foreignStatistics['curedCount']) + '\n'
-        results += '死亡病例：' + str(foreignStatistics['deadCount']) + '\n'
+        results += '[治愈病例]：' + str(foreignStatistics['curedCount']) + '\n'
+        results += '[死亡病例]：' + str(foreignStatistics['deadCount']) + '\n'
         # 全球疫情
         results += '\n======全球疫情======\n\n'
         globalStatistics = desc['globalStatistics']
-        results += '累计确诊：' + str(globalStatistics['confirmedCount']) + '\n'
-        results += '现存确诊：' + \
+        results += '[累计确诊]：' + str(globalStatistics['confirmedCount']) + '\n'
+        results += '[现存确诊]：' + \
             str(globalStatistics['currentConfirmedCount']) + '\n'
-        results += '治愈病例：' + str(globalStatistics['curedCount']) + '\n'
-        results += '死亡病例：' + str(globalStatistics['deadCount']) + '\n'
+        results += '[治愈病例]：' + str(globalStatistics['curedCount']) + '\n'
+        results += '[死亡病例]：' + str(globalStatistics['deadCount']) + '\n'
     print(results)
     return results
