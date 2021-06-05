7fae5680d9a5b75c469e1469b5b1d07332607346
uxiahnan <uxiahnan@outlook.com>
2021-06-05 17:20:04 +0800

    调整内容结构

--- kaptreebot/plugins/group_admin.py
+++ kaptreebot/plugins/group_admin.py
@@ -1,11 +1,20 @@
 from nonebot import on_notice
 from nonebot.adapters.cqhttp.bot import Bot
-from nonebot.adapters.cqhttp.event import GroupIncreaseNoticeEvent,GroupUploadNoticeEvent,GroupDecreaseNoticeEvent,GroupRequestEvent
+from nonebot.adapters.cqhttp.event import GroupIncreaseNoticeEvent
+from nonebot.adapters.cqhttp.event import GroupUploadNoticeEvent
+from nonebot.adapters.cqhttp.event import GroupDecreaseNoticeEvent
+from nonebot.adapters.cqhttp.event import GroupRecallNoticeEvent
+
+import pandas as pd
+import os
+import random
 
 # 将函数注册为群成员增加通知处理器
 
 # 入群提醒
 increase = on_notice()
+
+
 @increase.handle()
 async def increase(bot: Bot, event: GroupIncreaseNoticeEvent):
     if event.get_user_id != event.self_id:
@@ -19,6 +28,8 @@ async def increase(bot: Bot, event: GroupIncreaseNoticeEvent):
 
 # 退群提醒
 decrease = on_notice()
+
+
 @decrease.handle()
 async def decrease(bot: Bot, event: GroupDecreaseNoticeEvent):
     if event.get_user_id != event.self_id:
@@ -31,6 +42,8 @@ async def decrease(bot: Bot, event: GroupDecreaseNoticeEvent):
 
 # 上传提醒
 upload = on_notice()
+
+
 @upload.handle()
 async def upload(bot: Bot, event: GroupUploadNoticeEvent):
     if event.get_user_id != event.self_id:
@@ -38,5 +51,24 @@ async def upload(bot: Bot, event: GroupUploadNoticeEvent):
         await bot.send(
             event=event,
             message=msg,
+            at_sender=True
+        )
+
+# 撤回提醒
+ch = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
+                 sep=' ', encoding='utf-8')
+
+chehui = on_notice()
+
+
+@chehui.handle()
+async def cheh(bot: Bot, event: GroupRecallNoticeEvent):
+    if event.get_user_id != event.self_id:
+        k = (random.randint(1, 10000)) % len(ch)
+        result = ch.loc[k]['chehui']
+        print(result)
+        await bot.send(
+            event=event,
+            message=result,
             at_sender=True
         )
--- kaptreebot/plugins/pokeme.py
+++ kaptreebot/plugins/pokeme.py
@@ -1,22 +1,24 @@
-import random
+
 from nonebot import on_notice
 from nonebot.adapters.cqhttp.bot import Bot
-from nonebot.adapters.cqhttp.event import Event, PokeNotifyEvent, LuckyKingNotifyEvent, GroupRecallNoticeEvent
-from nonebot.adapters.cqhttp.message import Message
+from nonebot.adapters.cqhttp.event import PokeNotifyEvent, LuckyKingNotifyEvent
 from aiocqhttp import MessageSegment
 
 import pandas as pd
 import os
+import random
 
 pk = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/poke.txt',
-                sep=' ', encoding='utf-8')
+                 sep=' ', encoding='utf-8')
 
 pre = 0
 poke = on_notice()
+
+
 @poke.handle()
 async def _(bot: Bot, event: PokeNotifyEvent):
     if event.is_tome() and event.get_user_id != event.self_id:
-        k = (random.randint(1,10000))%len(pk)                
+        k = (random.randint(1, 10000)) % len(pk)
         result = pk.loc[k]['poke']
         print(result)
         await bot.send(
@@ -25,23 +27,9 @@ async def _(bot: Bot, event: PokeNotifyEvent):
             at_sender=True
         )
 
-ch = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
-                sep=' ', encoding='utf-8')
-
-chehui = on_notice()
-@chehui.handle()
-async def cheh(bot: Bot, event: GroupRecallNoticeEvent):
-    if event.get_user_id != event.self_id:
-        k = (random.randint(1,10000))%len(ch)                
-        result = ch.loc[k]['chehui']
-        print(result)
-        await bot.send(
-            event=event,
-            message=result,
-            at_sender=True
-        )
-
 regbag = on_notice()
+
+
 @regbag.handle()
 async def redb(bot: Bot, event: LuckyKingNotifyEvent):
     atmsg = MessageSegment.at(event.target_id)
