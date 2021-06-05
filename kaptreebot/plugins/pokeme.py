7e26e26b07bff84191910a1bfba36a8ac224ad19
xhnan <xhnan@epoint.com.cn>
2021-06-05 11:37:32 +0800

    调整格式及代码

--- kaptreebot/plugins/pokeme.py
+++ kaptreebot/plugins/pokeme.py
@@ -8,9 +8,8 @@ from nonebot.adapters.cqhttp.event import Event, PokeNotifyEvent, LuckyKingNotif
 from nonebot.adapters.cqhttp.message import Message
 from aiocqhttp import MessageSegment
 
-a = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/poke.txt',
+pk = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/poke.txt',
                 sep=' ', encoding='utf-8')
-print(a)
 
 pre = 0
 poke = on_notice()
@@ -18,31 +17,23 @@ poke = on_notice()
 async def _(bot: Bot, event: Event):
     if isinstance(event, PokeNotifyEvent):
         if event.is_tome() and event.get_user_id != event.self_id:
-            l = len(a)
-            k = random.randint(0, l-1)
-            while pre == k:
-                k = random.randint(0, l-1)
-            last = k
-            result = a[k]
-            print(result)
+            k = (random.randint(1,10000))%len(pk)                
+            result = pk.loc[k]['poke']
             await bot.send(
                 event=event,
                 message=result,
                 at_sender=True
             )
 
-b = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
+ch = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
                 sep=' ', encoding='utf-8')
-print(b)
 
 chehui = on_notice()
 @chehui.handle()
 async def cheh(bot: Bot, event: GroupRecallNoticeEvent):
     if event.get_user_id != event.self_id:
-        l = len(b)
-        k = random.randint(0, l-1)
-        result = b[k]
-        print(result)
+        k = (random.randint(1,10000))%len(ch)                
+        result = ch.loc[k]['chehui']
         await bot.send(
             event=event,
             message=result,
