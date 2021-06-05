96306bb4ef1f41efe0c8113af9fb86446d94f34f
uxiahnan <uxiahnan@outlook.com>
2021-06-06 01:57:45 +0800

    上传文件问题debug

--- kaptreebot/plugins/group_admin.py
+++ kaptreebot/plugins/group_admin.py
@@ -47,7 +47,9 @@ upload = on_notice()
 @upload.handle()
 async def upload(bot: Bot, event: GroupUploadNoticeEvent):
     if event.get_user_id != event.self_id:
-        msg = '有人上传了：' + event.file.name + '好像打开看看呢~',
+        print(event.file.name)
+        msg = '有人上传了：' + event.file.name + '好像打开看看呢~'
+        print(msg)
         await bot.send(
             event=event,
             message=msg,
