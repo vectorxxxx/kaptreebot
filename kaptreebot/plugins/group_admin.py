8f030973c6e90620247acacb5b908e1600cfa5d4
xhnan <xhnan@epoint.com.cn>
2021-06-05 16:25:31 +0800

    群消息提醒

--- kaptreebot/plugins/group_admin.py
+++ kaptreebot/plugins/group_admin.py
@@ -1,8 +1,42 @@
-from nonebot import on_notice, NoticeSession
-
+from nonebot import on_notice
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.event import GroupIncreaseNoticeEvent,GroupUploadNoticeEvent,GroupDecreaseNoticeEvent,GroupRequestEvent
 
 # 将函数注册为群成员增加通知处理器
-@on_notice('group_increase')
-async def _(session: NoticeSession):
-    # 发送欢迎消息
-    await session.send('哇~是新的rbq！')
+
+# 入群提醒
+increase = on_notice()
+@increase.handle()
+async def increase(bot: Bot, event: GroupIncreaseNoticeEvent):
+    if event.get_user_id != event.self_id:
+        msg = event.operator_id + '哇~是新的rbq！'
+        await bot.send(
+            event=event,
+            message=msg,
+            at_sender=True
+        )
+
+
+# 退群提醒
+decrease = on_notice()
+@decrease.handle()
+async def decrease(bot: Bot, event: GroupDecreaseNoticeEvent):
+    if event.get_user_id != event.self_id:
+        msg = event.operator_id + '离开了，好难过~'
+        await bot.send(
+            event=event,
+            message=msg,
+            at_sender=True
+        )
+
+# 上传提醒
+upload = on_notice()
+@upload.handle()
+async def upload(bot: Bot, event: GroupUploadNoticeEvent):
+    if event.get_user_id != event.self_id:
+        msg = '有人上传了：' + event.file.name + '好像打开看看呢~',
+        await bot.send(
+            event=event,
+            message=msg,
+            at_sender=True
+        )
--- kaptreebot/plugins/pokeme.py
+++ kaptreebot/plugins/pokeme.py
@@ -14,19 +14,16 @@ pk = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/poke.txt',
 pre = 0
 poke = on_notice()
 @poke.handle()
-async def _(bot: Bot, event: Event):
-    print("11111111111111")
-    if isinstance(event, PokeNotifyEvent):
-        print("22222222222222") 
-        if event.is_tome() and event.get_user_id != event.self_id:
-            k = (random.randint(1,10000))%len(pk)                
-            result = pk.loc[k]['poke']
-            print(result)
-            await bot.send(
-                event=event,
-                message=result,
-                at_sender=True
-            )
+async def _(bot: Bot, event: PokeNotifyEvent):
+    if event.is_tome() and event.get_user_id != event.self_id:
+        k = (random.randint(1,10000))%len(pk)                
+        result = pk.loc[k]['poke']
+        print(result)
+        await bot.send(
+            event=event,
+            message=result,
+            at_sender=True
+        )
 
 ch = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
                 sep=' ', encoding='utf-8')
@@ -34,7 +31,6 @@ ch = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui.txt',
 chehui = on_notice()
 @chehui.handle()
 async def cheh(bot: Bot, event: GroupRecallNoticeEvent):
-    print(event.get_user_id + "===="+event.self_id)
     if event.get_user_id != event.self_id:
         k = (random.randint(1,10000))%len(ch)                
         result = ch.loc[k]['chehui']
