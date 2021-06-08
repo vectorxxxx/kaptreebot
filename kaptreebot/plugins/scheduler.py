225b8516fd10b7c9c9462462f385c595df121869
uxiahnan <uxiahnan@outlook.com>
2021-06-09 02:39:24 +0800

    调试定时任务

--- kaptreebot/plugins/scheduler.py
+++ kaptreebot/plugins/scheduler.py
@@ -1,15 +1,22 @@
 from nonebot import require
+from aiocqhttp import MessageSegment
 import nonebot
+import os
 
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 
-@scheduler.scheduled_job('cron', hour='2', minute='25', id='drink_tea')
-async def day_limits():
+@scheduler.scheduled_job('cron', hour='2', minute='41', id='goodmorning')
+async def good_morning():
     (bot,) = nonebot.get_bots().values()
     if bot is not None:
+        imgpath = 'file:///'+os.getcwd()+'/data/img/hello.gif'
+        msg = '[CQ:at,qq="全体人员"] 早上好呀，小哥哥们~\n' + MessageSegment.image(imgpath)
         await bot.send_msg(
-            message_type="private", user_id='1402758731', message='好困啊~'
+            message_type="group",
+            group_id=582597352,
+            message='好困啊~'
         )
 
-scheduler.add_job(day_limits,  "interval", days=1, id="xxx")
+
+scheduler.add_job(good_morning,  "interval", days=1, id="goodmorning")
