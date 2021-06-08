99bbd1742099f7798c3e281ebaf4d9116b4ad390
uxiahnan <uxiahnan@outlook.com>
2021-06-09 01:57:50 +0800

    调试定时任务

--- kaptreebot/plugins/scheduler.py
+++ kaptreebot/plugins/scheduler.py
@@ -1,35 +1,31 @@
-import asyncio
 from concurrent.futures import ProcessPoolExecutor
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from nonebot.adapters.cqhttp import Bot, Event
+import asyncio
+import nonebot
 
 
-async def day_limits(bot: Bot, event: Event):
-    await bot.send(
-        event=event,
-        message='好困啊',
-        kwargs={'user_id','1402758731'}
+async def day_limits():
+    bot = nonebot.get_bots()
+    await bot.send_msg(
+        message_type="private", user_id='1402758731', message='好困啊~'
     )
 
 
-if __name__ == "__main__":
+scheduler = AsyncIOScheduler()
+scheduler.add_job(day_limits, 'cron', hour=2, minute=0,
+                  misfire_grace_time=3600, timezone='Asia/Shanghai')
 
-    variable = 60
+scheduler.start()
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(day_limits, 'cron', hour=1, minute=10,
-                      misfire_grace_time=3600, timezone='Asia/Shanghai', kwargs={Bot, Event})
+scheduler.print_jobs()
 
-    scheduler.start()
+executor = ProcessPoolExecutor(1)
+loop = asyncio.get_event_loop()
 
-    scheduler.print_jobs()
+try:
+    loop.run_forever()
 
-    executor = ProcessPoolExecutor(1)
-    loop = asyncio.get_event_loop()
-
-    try:
-        loop.run_forever()
-
-    except (KeyboardInterrupt, Exception):
-        loop.stop()
-        scheduler.shutdown()
+except (KeyboardInterrupt, Exception):
+    loop.stop()
+    scheduler.shutdown()
