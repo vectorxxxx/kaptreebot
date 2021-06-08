afa5923a420922d9eaaa3f9f2d1a84ad451130ad
uxiahnan <uxiahnan@outlook.com>
2021-06-09 03:25:56 +0800

    test

--- kaptreebot/plugins/scheduler.py
+++ kaptreebot/plugins/scheduler.py
@@ -1,17 +1,32 @@
+import json
+import requests
+from commons import property
 from nonebot import require
 from aiocqhttp import MessageSegment
 import nonebot
 import os
 
+
+# 要操作的properties文件的路径
+file_path = os.getcwd() + '/properties/cheat/others.properties'
+# 读取文件
+props = property.parse(file_path)
+tianxing_api = props.get('tianxing_api')
+tianxing_key2 = props.get('tianxing_key2')
+
+
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 
-@scheduler.scheduled_job('cron', hour='3', minute='8', id='goodm')
-async def day_limits():
+# ===========早安===========
+@scheduler.scheduled_job('cron', hour='3', minute='30', id='goodmorning')
+async def good_morning():
     (bot,) = nonebot.get_bots().values()
     if bot is not None:
         imgpath = 'file:///'+os.getcwd()+'/data/img/hello.gif'
-        msg = '[CQ:at,qq="全体人员"] 早上好呀，小哥哥们~\n' + MessageSegment.image(imgpath)
+        msg = '[CQ:at,qq="全体人员"] \n'
+        msg += get_zaoan()
+        msg += MessageSegment.image(imgpath)
         await bot.send_msg(
             message_type="group",
             group_id=582597352,
@@ -19,4 +34,14 @@ async def day_limits():
         )
 
 
-# scheduler.add_job(day_limits,  "interval", days=1, id="goodm")
+def get_zaoan():
+    url = tianxing_api + 'zaoan/index?key=' + tianxing_key2
+    res = requests.get(url)
+    c = json.loads(res.text)
+    if c['code'] != 200:
+        return '早上好呀，小哥哥们~'
+    result = ''
+    for news in c['newslist']:
+        result = news['content']
+        print(result)
+        return result
