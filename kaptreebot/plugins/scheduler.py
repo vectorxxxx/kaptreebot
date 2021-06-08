612957635d7ba6f02809433ad015fe018716f016
uxiahnan <uxiahnan@outlook.com>
2021-06-09 02:03:38 +0800

    调试定时任务

--- kaptreebot/plugins/scheduler.py
+++ kaptreebot/plugins/scheduler.py
@@ -6,14 +6,14 @@ import nonebot
 
 
 async def day_limits():
-    bot = nonebot.get_bots()
+    (bot,) = nonebot.get_bots().values()
     await bot.send_msg(
         message_type="private", user_id='1402758731', message='好困啊~'
     )
 
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(day_limits, 'cron', hour=2, minute=0,
+scheduler.add_job(day_limits, 'cron', hour=2, minute=5,
                   misfire_grace_time=3600, timezone='Asia/Shanghai')
 
 scheduler.start()
