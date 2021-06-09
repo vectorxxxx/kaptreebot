0c6a6f0672b3a5e8f73bd5e5c745d6de1882b790
xhnan <xhnan@epoint.com.cn>
2021-06-09 11:43:24 +0800

    。。。

--- kaptreebot/plugins/group_admin.py
+++ kaptreebot/plugins/group_admin.py
@@ -23,7 +23,7 @@ urllib3.disable_warnings()
 
 # 获取头像
 def get_tx(qq):
-    print('qq='+qq)
+    print('qq='+str(qq))
     url = 'https://api.ghser.com/qq/?get=%s' % str(qq)
     res = requests.get(url.encode('utf-8'), verify=False)
     print(res.history)
@@ -37,7 +37,7 @@ def get_tx(qq):
 
 # 获取昵称
 def get_name(qq):
-    print('qq='+qq)
+    print('qq='+str(qq))
     url = 'https://api.ghser.com/qq/?get==%s' % str(qq)
     res = requests.get(url.encode('utf-8'),verify=False)
     print(res.history)
