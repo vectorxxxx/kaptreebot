fbda9519bc9ddee0c774802618290eeb066b0ce9
xhnan <xhnan@epoint.com.cn>
2021-06-09 10:42:42 +0800

    修改json问题

--- kaptreebot/plugins/group_admin.py
+++ kaptreebot/plugins/group_admin.py
@@ -23,7 +23,7 @@ chehui_tome = pd.read_csv('file:///' + os.getcwd() + '/data/pokeme/chehui_tome.t
 def get_tx(qq):
     url = 'https://api.ghser.com/qq/?get=' + str(qq)
     res = requests.get(url,verify=False)
-    c = json.loads(res.text)
+    c = json.loads(res)
     if not c['success']:
         return ''
     imgurl = c['imgurl']
@@ -34,7 +34,7 @@ def get_tx(qq):
 def get_name(qq):
     url = 'https://api.ghser.com/qq/?get=' + str(qq)
     res = requests.get(url,verify=False)
-    c = json.loads(res.text)
+    c = json.loads(res)
     if not c['success']:
         return ''
     name = c['name']
