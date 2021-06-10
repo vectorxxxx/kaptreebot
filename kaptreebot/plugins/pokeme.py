5e6fce19fd02f1657e2539d201000916dafb0afe
xhnan <xhnan@epoint.com.cn>
2021-06-10 13:09:52 +0800

    修复戳一戳bug

--- kaptreebot/plugins/pokeme.py
+++ kaptreebot/plugins/pokeme.py
@@ -2,7 +2,7 @@
 from nonebot import on_notice, on_command
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import PokeNotifyEvent
-from nonebot.adapters.cqhttp.message import MessageSegment
+from aiocqhttp import MessageSegment
 import pandas as pd
 import os
 import random
@@ -18,7 +18,7 @@ poke = on_notice()
 async def _(bot: Bot, event: PokeNotifyEvent):
     if event.is_tome() and event.get_user_id != event.self_id:
         msg = ''
-        ran_int = random.randint(0, 2)
+        ran_int = random.randint(0, 1)
         if ran_int == 0:
             k = (random.randint(1, 10000)) % len(pk)
             msg = pk.loc[k]['poke']
@@ -28,14 +28,8 @@ async def _(bot: Bot, event: PokeNotifyEvent):
                 message=msg,
                 at_sender=True
             )
-        elif ran_int == 1:
-            msg = MessageSegment.poke()
-            await bot.send(
-                event=event,
-                message=msg
-            )
         else:
-            msg = MessageSegment.shake()
+            msg = MessageSegment.poke(id_=event.get_user_id)
             await bot.send(
                 event=event,
                 message=msg
